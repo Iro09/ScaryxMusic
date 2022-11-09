4399d9923cff6e6cdd90103fb3c8a1d74ad07f15
Lakshya Maheshwari <116993014+LaKsH-X@users.noreply.github.com>
2022-11-09 09:27:42 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("AnonAssociation")
+                await self.one.join_chat("xSupport_ies")
+                await self.one.join_chat("Scary_support")
             except:
                 pass
             assistants.append(1)
@@ -81,8 +81,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("AnonAssociation")
+                await self.two.join_chat("xSupport_ies")
+                await self.two.join_chat("Scary_Support")
             except:
                 pass
             assistants.append(2)
@@ -111,8 +111,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("AnonAssociation")
+                await self.three.join_chat("xSupport_ies")
+                await self.three.join_chat("Scary_Support")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("AnonAssociation")
+                await self.four.join_chat("xSupport_ies")
+                await self.four.join_chat("Scary_Support")
             except:
                 pass
             assistants.append(4)
@@ -171,8 +171,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("AnonAssociation")
+                await self.five.join_chat("xSupport_ies")
+                await self.five.join_chat("Scary_Support")
             except:
                 pass
             assistants.append(5)
